c2e1ab2d2e444b4395e8e4cb6578946c6d3765c3
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-05-30 10:16:10 +0900

    First Version

---
+++ effects.js
@@ -0,0 +1,16 @@
+let sideMenu = document.getElementById("side-menu");
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-200px"
+}
+
+const submitButton = document.getElementById("submit-btn")
+const formReset = document.getElementById("myForm")
+
+
+
+
